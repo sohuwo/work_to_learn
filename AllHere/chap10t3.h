009a95317a43efeb9009674637e8525aedb10ecd
创意 <sohuwo@yeah.net>
2019-07-09 09:55:18 +0800

    chap10t3 going

---
+++ AllHere/chap10t3.h
@@ -0,0 +1,22 @@
+//Classifies a poker hand
+#ifndef CHAP10T3_H
+#define CHAP10T3_H
+
+#include <stdbool.h>
+void chap10t3(void);
+
+#define NUM_RANKS 13 
+#define NUM_SUITS 4
+#define NUM_CARDS 5
+
+#define SUIT_AND_RANK 2
+
+//external variables
+bool straight, flush, four, three;
+int pairs;//can be 0,1,2
+
+void read_cards(int hand[][SUIT_AND_RANK],int n);
+void analyse_hand(int hand[][SUIT_AND_RANK],int n);
+void print_result(void);
+
+#endif

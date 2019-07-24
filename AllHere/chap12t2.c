89c61b7f9844f32c84e37c0cd02efcc1adfa8e18
创意 <sohuwo@yeah.net>
2019-07-24 16:25:53 +0800

    chapter 12 b

--- AllHere/chap12t2.c
+++ AllHere/chap12t2.c
@@ -4,7 +4,7 @@
 
 #define MSG_LEN 20
 
-void chap12t2(void)
+void chap12t2a(void)
 {
 	char message[MSG_LEN], ch;
 	int i, j;
@@ -21,6 +21,30 @@ void chap12t2(void)
 	for (j = 0; j <= i; j++, i--)
 	{
 		if (message[j] != message[i])
+		{
+			printf("Not a palindrome\n");
+			return;
+		}
+	}
+	printf("Palindrome\n");
+}
+
+void chap12t2(void)
+{
+	char message[MSG_LEN], ch,*high,*low;
+	printf("Enter a message:");
+	for (high = message; high < message + MSG_LEN;)
+	{
+		ch = getchar();
+		if (ch == '\n')
+			break;
+		if (isalpha(ch))
+			*high++ = tolower(ch);
+	}
+	high--;
+	for (low = message; low <= high; low++, high--)
+	{
+		if (*low != *high)
 		{
 			printf("Not a palindrome\n");
 			return;
